4637ed805ca295a2d5513e06f8619acb1b6ff6b7
Enas <enaselmahdwi@gmail.com>
2021-11-04 13:26:18 -0600

    Enas-Elmahdwi

--- app/src/components/Person.js
+++ app/src/components/Person.js
@@ -20,19 +20,14 @@ const Person = ({ person, isFetching, error, dispatch }) => {
 
   const handleClick = () => {
     dispatch(getPerson());
-    // dispatch(fetchError("this causes an eror now"));
-
-    //0. connect our actions to this component
-    //1. make a api call to https://randomuser.me/api/
-    //2. if call is successful: dispatch fetchSuccess passing in our person
-    //3. if call is not successful: dispatch fetchError passing in error message
+   
   }
 
   return (
     <>
       <div>
-        <h2>Say Hi to: {person.name.first} {person.name.last}</h2>
-        <img src={person.picture.large}/>
+        <h2>Say Hi to: {person.age} {person.count}</h2>
+        <img src={person.name}/>
       </div>
       <button onClick={handleClick}>Get new person</button>
     </>
