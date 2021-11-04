40167218b271ebc1eda60168538e02a812d85a21
Enas <enaselmahdwi@gmail.com>
2021-11-03 18:19:02 -0600

    build files and fetch data

---
+++ app/src/components/Person.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchError, getPerson } from '../actions';
+
+const Person = ({ person, isFetching, error, dispatch }) => {
+
+  useEffect(()=> {
+    dispatch(getPerson());
+  }, []);
+
+  if (error) {
+    return <h2>We got an error: {error}</h2>;
+  }
+
+  if (isFetching) {
+    return <h2>Fetching person for ya!</h2>;
+  }
+
+
+  const handleClick = () => {
+    dispatch(getPerson());
+    
+  }
+
+  return (
+    <>
+      <div>
+        <h2>Say Hi to: {person.name} {person.name.age}</h2>
+      
+      </div>
+      <button onClick={handleClick}>Get new person</button>
+    </>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    person: state.person,
+    isFetching: state.isFetching,
+    error: state.error
+  };
+};
+
+export default connect(mapStateToProps)(Person);
